ebcd188a64356a4daedb0b03c0c3cd6a768778aa
Prefeitura Municipal de Campo Largo <analistas@campolargo.pr.gov.br>
2025-10-06 11:50:49 -0300

    Update local_define.php

--- local_define.php
+++ local_define.php
@@ -1,4 +1,5 @@
 <?php
-define('GLPI_VAR_DIR', '/var/lib/glpi/');
-define('GLPI_LOG_DIR', '/var/log/glpi/');
+define('GLPI_VAR_DIR', '/var/lib/glpi/files');
+define('GLPI_LOG_DIR', '/var/log/glpi');
+define('GLPI_MARKETPLACE_DIR', '/var/lib/glpi/plugins');
 ?>
